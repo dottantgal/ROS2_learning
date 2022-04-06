71e1a4e8fdc2c0a37fb0da2f19f99b1038b6a470
Antonio Mauro Galiano <dottantgal@users.noreply.github.com>
2022-04-06 19:21:17 +0200

    Update my_first_node.cpp

--- CPP/01 Just to start/my_first_node.cpp
+++ CPP/01 Just to start/my_first_node.cpp
@@ -1,14 +1,30 @@
-#include "rclcpp/rclcpp.hpp"    // import of rclcpp library to retrieve many of the ROS2 core functionalities: nodes, topics, services
+/**
+ * @file my_first_node.cpp
+ *
+ * @brief A basic ROS2 node that spins and printout a string
+ *        waiting for a killing request
+ *
+ * @author Antonio Mauro Galiano
+ * Contact: https://www.linkedin.com/in/antoniomaurogaliano/
+ *
+ */
+
+
+// import of rclcpp library to retrieve many of the ROS2 core functionalities: nodes, topics, services
+#include "rclcpp/rclcpp.hpp"
+
 
 int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);   // initiate ROS2 communications
 
-  auto node = std::make_shared<rclcpp::Node>("ros2_node");    // creation of the ROS2 node handled by a shared_ptr
-                                                              // to avoid use of new/delete thanks to smart pointers
-                                                              // auto is equivalent to std::shared_ptr<rclcpp::Node>
+  // creation of the ROS2 node handled by a shared_ptr
+  // to avoid use of new/delete thanks to smart pointers
+  // auto is equivalent to std::shared_ptr<rclcpp::Node>
+  auto node = std::make_shared<rclcpp::Node>("ros2_node");
 
-  RCLCPP_INFO(node->get_logger(), "ROS2 node first test");    //  use of the the logger method of the class Node to printout info
+  // use of the the logger method of the class Node to printout info
+  RCLCPP_INFO(node->get_logger(), "ROS2 node first test");
   rclcpp::spin(node);   // the node spins until a killing request (Ctrl^C)
   rclcpp::shutdown();   // shutdown of the node
   return 0;   // return to the operating system
