91135d6390996d1c2e6bbd65353c23530c4bf785
Antonio Mauro Galiano <dottantgal@users.noreply.github.com>
2022-04-06 16:10:25 +0200

    Create sub_pub_pipeline.cpp

---
+++ CPP/02 Publisher and subscriber/sub_pub_pipeline.cpp
@@ -0,0 +1,46 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+
+class SubPubPipeline : public rclcpp::Node
+{
+private:
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;    // definition of the publisher of a String type message
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_;     // definition of the subscriber to a String type message
+  std::string topicName_;
+  std::string modTopicName_;
+  void TopicCallback(const std_msgs::msg::String::SharedPtr msg);
+
+public:
+    SubPubPipeline(std::string passedNodeName="/VOID", std::string passedTopicName="/VOID", std::string passedModTopicName="/VOID") 
+    : Node(passedNodeName), topicName_(passedTopicName), modTopicName_(passedModTopicName)
+    {
+        pub_ = this->create_publisher<std_msgs::msg::String>(modTopicName_, 10);    // definition of the publisher of a new topic
+        sub_ = this->create_subscription<std_msgs::msg::String>(
+            topicName_, 10, std::bind(&SubPubPipeline::TopicCallback, 
+            this, std::placeholders::_1));    // definitio of the subscriber to the original topic
+                                              // placeholder to pass the topic message data to the callback
+    }    
+};
+
+
+void SubPubPipeline::TopicCallback(const std_msgs::msg::String::SharedPtr msg)
+{
+    auto newMsg = std_msgs::msg::String();
+    //    the line above can be written as std_msgs::msg::String newMsg;
+    newMsg.data = "I RESEND " + msg->data;    // new message data is created appending the message got from the topic
+    RCLCPP_INFO(this->get_logger(), 
+      "Received: %s, Published: %s", msg->data.c_str(), newMsg.data.c_str());   // look that an arrow operator is used
+                                                                                // for the msg cause it's a pointer
+    pub_->publish(newMsg);    // the modified message is published on a new topic
+}
+
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<SubPubPipeline>("my_pipeline_node", "/my_message", "/my_mod_message");
+    rclcpp::spin(node);   // the node spins doing nothing until a message is published on the proper topic 
+    rclcpp::shutdown();
+    return 0;
+}
