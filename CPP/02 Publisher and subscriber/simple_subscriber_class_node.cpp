c8ec6dd388e0db1497a9de8a193777806657865a
Antonio Mauro Galiano <dottantgal@users.noreply.github.com>
2022-04-06 19:16:22 +0200

    Update simple_subscriber_class_node.cpp

--- CPP/02 Publisher and subscriber/simple_subscriber_class_node.cpp
+++ CPP/02 Publisher and subscriber/simple_subscriber_class_node.cpp
@@ -1,21 +1,34 @@
+/**
+ * @file simple_subscriber_class_node.cpp
+ *
+ * @brief A basic subscriber class node
+ *
+ * @author Antonio Mauro Galiano
+ * Contact: https://www.linkedin.com/in/antoniomaurogaliano/
+ *
+ */
+
+
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+
 
 class MySubscriber : public rclcpp::Node
 {
 private:
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_;    // definition of the subscriber to a String type message
-  void SubscriberCallback(const std_msgs::msg::String::SharedPtr msg);    // subscriber callback triggered when a new message
-                                                                          // is published on the /my_message topic
+  // subscriber callback triggered when a new message is published to the /my_message topic
+  void SubscriberCallback(const std_msgs::msg::String::SharedPtr msg);
 
 public:
   MySubscriber(std::string passedNodeName="VOID") : Node(passedNodeName)
   {
+    // the subscriber declaration which waits messages and triggers the SubscriberCallback
+    // when a message is available on the /my_message topic.
+    // the binding needs a placeholders to pass the topic message data to the callback
     sub_ = this->create_subscription<std_msgs::msg::String>(
-      "/my_message", 10, std::bind(&MySubscriber::SubscriberCallback, 
-      this, std::placeholders::_1));    // the subscriber declaration which waits messages and triggers the SubscriberCallback
-                                        // when a message is available on the /my_message topic.
-                                        // the binding needs a placeholders to pass the topic message data to the callback
+      "/my_message", 10, std::bind(&MySubscriber::SubscriberCallback,
+      this, std::placeholders::_1));
   }
 
 };
@@ -32,8 +45,8 @@ int main(int argc, char * argv[])
   rclcpp::init(argc, argv);
   auto node = std::make_shared<MySubscriber>("my_subscriber_node");
   rclcpp::spin(node);   // the subscriber spins waiting for messages doing nothing until a message is available
-  //    it's possible to define the two rows above as
-  //    rclcpp::spin(std::make_shared<MySubscriber>("my_subscriber_node"));
+  // it's possible to define the two rows above as
+  // eclcpp::spin(std::make_shared<MySubscriber>("my_subscriber_node"));
   rclcpp::shutdown();
   return 0;
 }
