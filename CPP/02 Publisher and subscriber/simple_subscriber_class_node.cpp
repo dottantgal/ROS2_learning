e90728e924cd11214f9e06040310b52b0ec1011e
Antonio Mauro Galiano <dottantgal@users.noreply.github.com>
2022-04-08 14:15:03 +0200

    Update simple_subscriber_class_node.cpp

--- CPP/02 Publisher and subscriber/simple_subscriber_class_node.cpp
+++ CPP/02 Publisher and subscriber/simple_subscriber_class_node.cpp
@@ -16,14 +16,15 @@
 class MySubscriber : public rclcpp::Node
 {
 private:
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_;    // definition of the subscriber to a String type message
+  // declaration of the subscriber to a String type message
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_;
   // subscriber callback triggered when a new message is published to the /my_message topic
   void SubscriberCallback(const std_msgs::msg::String::SharedPtr msg);
 
 public:
   MySubscriber(std::string passedNodeName="VOID") : Node(passedNodeName)
   {
-    // the subscriber declaration which waits messages and triggers the SubscriberCallback
+    // the subscriber definition which waits messages and triggers the SubscriberCallback
     // when a message is available on the /my_message topic.
     // the binding needs a placeholders to pass the topic message data to the callback
     sub_ = this->create_subscription<std_msgs::msg::String>(
@@ -44,9 +45,10 @@ int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   auto node = std::make_shared<MySubscriber>("my_subscriber_node");
-  rclcpp::spin(node);   // the subscriber spins waiting for messages doing nothing until a message is available
+  // the subscriber spins waiting for messages doing nothing until a message is available
+  rclcpp::spin(node);
   // it's possible to define the two rows above as
-  // eclcpp::spin(std::make_shared<MySubscriber>("my_subscriber_node"));
+  // rclcpp::spin(std::make_shared<MySubscriber>("my_subscriber_node"));
   rclcpp::shutdown();
   return 0;
 }
