f2accddf1f5e03dee18e3705bb8d0e0ab29986fd
Antonio Mauro Galiano <dottantgal@users.noreply.github.com>
2022-04-09 12:11:08 +0200

    Create client_node_class.cpp

---
+++ CPP/04 Service and client/client_node_class.cpp
@@ -0,0 +1,91 @@
+/**
+ * @file client_node_class.cpp
+ *
+ * @brief A basic ROS2 service client node with class implementation that asks the user 
+ *        to input twostrings and gets back a capitalized full string from the server service.
+ *        It's necessary to use the custom message defined in the external 
+ *        package "Custom msg and srv"
+ *
+ * @author Antonio Mauro Galiano
+ * Contact: https://www.linkedin.com/in/antoniomaurogaliano/
+ *
+ */
+
+
+#include "custom_srv_msg/srv/capital_full_name.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+
+using namespace std::chrono_literals;
+
+
+class MyClientNode : public rclcpp::Node
+{
+private:
+  const std::string name_;
+  const std::string surname_;
+  rclcpp::Client<custom_srv_msg::srv::CapitalFullName>::SharedPtr client_;
+
+public:
+  MyClientNode(std::string passedNodeName="VOID", std::string passedName="VOID", std::string passedSurname="VOID")
+    : Node(passedNodeName), name_(passedName), surname_(passedSurname)
+  {
+    client_ = this->create_client<custom_srv_msg::srv::CapitalFullName>("create_cap_full_name");
+  }
+  int ServerResponse();
+};
+
+
+int MyClientNode::ServerResponse()
+{
+  auto request =
+    std::make_shared<custom_srv_msg::srv::CapitalFullName::Request>();
+
+  request->name = name_;
+  request->surname = surname_;
+  std::chrono::seconds myPause = 1s;
+
+  while (!client_->wait_for_service(1s)) 
+  {
+    if (!rclcpp::ok()) 
+    {
+      RCLCPP_ERROR(rclcpp::get_logger("rclcpp"),
+                   "Interrupted while waiting for the service. Exiting.");
+      return 0;
+    }
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"),
+                "SERVICE NOT AVAILABLE, waiting again...");
+  }
+
+  auto result = client_->async_send_request(request);
+
+  // note that for the MyClientNode object it's used the get_node_base_interface
+  // to allow the client spinning for a server response
+  // it returns the needed rclcpp::node_interfaces::NodeBaseInterface::SharedPtr 
+  if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), result) ==
+      rclcpp::executor::FutureReturnCode::SUCCESS)
+  {
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Capitalized full name: %s",
+                result.get()->capitalfullname.c_str());
+  } else
+  {
+    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"),
+                 "Failed to call service create_cap_full_name");
+  }
+  return 0;
+}
+
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  std::string name = "";
+  std::string surname = "";
+  std::cout << "Insert the name -> ";
+  std::cin >> name;
+  std::cout << "Insert the surname -> ";
+  std::cin >> surname;
+  auto node = std::make_shared<MyClientNode>("client_node", name, surname);
+  node->ServerResponse();
+  rclcpp::shutdown();
+}
