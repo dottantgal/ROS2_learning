025fe3e419573b778e1d9c06d7a129f297fd7f9b
Antonio Mauro Galiano <dottantgal@users.noreply.github.com>
2022-04-15 15:09:32 +0200

    Added the plugin package

---
+++ CPP/06 Plugins/vehicle_plugins/src/vehicle_plugins.cpp
@@ -0,0 +1,78 @@
+#include "vehicle_base/regular_vehicle.hpp"
+
+// derived classes declaration which will be instanciated through pluginlib
+namespace vehicle_plugins
+{
+  class Motorbike : public vehicle_base::RegularVehicle
+  {
+    public:
+      void initialize(std::string vehicleType) override
+      {
+        vehicleType_ = vehicleType;
+      }
+
+      int setNumPassengers() override
+      {
+        return 2;
+      }
+
+      std::string getVehicleType() override
+      {
+        return vehicleType_;
+      }
+
+    protected:
+      std::string vehicleType_;
+  };
+
+  class Bicycle : public vehicle_base::RegularVehicle
+  {
+    public:
+      void initialize(std::string vehicleType) override
+      {
+        vehicleType_ = vehicleType;
+      }
+
+      int setNumPassengers() override
+      {
+        return 1;
+      }
+
+      std::string getVehicleType() override
+      {
+        return vehicleType_;
+      }
+
+    protected:
+      std::string vehicleType_;
+  };
+
+  class Truck : public vehicle_base::RegularVehicle
+  {
+    public:
+      void initialize(std::string vehicleType) override
+      {
+        vehicleType_ = vehicleType;
+      }
+
+      int setNumPassengers() override
+      {
+        return 5;
+      }
+
+      std::string getVehicleType() override
+      {
+        return vehicleType_;
+      }
+
+    protected:
+      std::string vehicleType_;
+  };
+}
+
+#include <pluginlib/class_list_macros.hpp>
+
+// registration of the derived classes as plugin
+PLUGINLIB_EXPORT_CLASS(vehicle_plugins::Motorbike, vehicle_base::RegularVehicle)
+PLUGINLIB_EXPORT_CLASS(vehicle_plugins::Bicycle, vehicle_base::RegularVehicle)
+PLUGINLIB_EXPORT_CLASS(vehicle_plugins::Truck, vehicle_base::RegularVehicle)
