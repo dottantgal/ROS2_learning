ab7ad99248bc288a4e5e9c0af7f7728762c43394
Antonio Mauro Galiano <dottantgal@users.noreply.github.com>
2022-04-22 14:08:10 +0200

    Update class_action_server.cpp

--- CPP/07 Actions/action_tutorial/src/class_action_server.cpp
+++ CPP/07 Actions/action_tutorial/src/class_action_server.cpp
@@ -1,9 +1,8 @@
 /**
- * @file class_action_client.cpp
+ * @file class_action_server.cpp
  *
- * @brief A class defined ROS2 action client node that sends as goal the number of string 
- *        concatenation the action server should perform. 
- *        The server will send back feedbacks and the final result
+ * @brief A class defined ROS2 action server node that concatenates a string 
+ *         based on the number of string concatenation sent by a client within a goal request
  *
  * @author Antonio Mauro Galiano
  * Contact: https://www.linkedin.com/in/antoniomaurogaliano/
@@ -27,32 +26,32 @@ public:
   {
     using namespace std::placeholders;
 
-    this->action_server_ = rclcpp_action::create_server<Concatenate>(
+    this->actionServer_ = rclcpp_action::create_server<Concatenate>(
       this->get_node_base_interface(),
       this->get_node_clock_interface(),
       this->get_node_logging_interface(),
       this->get_node_waitables_interface(),
       "concatenation",
-      std::bind(&ConcatenateActionServer::handle_goal, this, _1, _2),
-      std::bind(&ConcatenateActionServer::handle_cancel, this, _1),
-      std::bind(&ConcatenateActionServer::handle_accepted, this, _1));
+      std::bind(&ConcatenateActionServer::HandleGoal, this, _1, _2),
+      std::bind(&ConcatenateActionServer::HandleCancel, this, _1),
+      std::bind(&ConcatenateActionServer::HandleAccepted, this, _1));
   }
 
 private:
-  rclcpp_action::Server<Concatenate>::SharedPtr action_server_;
-  rclcpp_action::GoalResponse handle_goal(
+  rclcpp_action::Server<Concatenate>::SharedPtr actionServer_;
+  rclcpp_action::GoalResponse HandleGoal(
     const rclcpp_action::GoalUUID & uuid,
     std::shared_ptr<const Concatenate::Goal> goal);
-  rclcpp_action::CancelResponse handle_cancel(
-    const std::shared_ptr<GoalHandleConcatenate> goal_handle);
-  void execute(const std::shared_ptr<GoalHandleConcatenate> goal_handle);
-  void handle_accepted(const std::shared_ptr<ConcatenateActionServer::GoalHandleConcatenate> goal_handle);
+  rclcpp_action::CancelResponse HandleCancel(
+    const std::shared_ptr<GoalHandleConcatenate> goalHandle);
+  void execute(const std::shared_ptr<GoalHandleConcatenate> goalHandle);
+  void HandleAccepted(const std::shared_ptr<ConcatenateActionServer::GoalHandleConcatenate> goalHandle);
 };
 
 
-rclcpp_action::GoalResponse ConcatenateActionServer::handle_goal(
+rclcpp_action::GoalResponse ConcatenateActionServer::HandleGoal(
     const rclcpp_action::GoalUUID & uuid,
-    std::shared_ptr<const ConcatenateActionServer::Concatenate::Goal> goal)
+    std::shared_ptr<const Concatenate::Goal> goal)
 {
   RCLCPP_INFO(rclcpp::get_logger("server"),
               "Got goal request with %d string concatenations",
@@ -67,21 +66,21 @@ rclcpp_action::GoalResponse ConcatenateActionServer::handle_goal(
 }
 
 
-rclcpp_action::CancelResponse ConcatenateActionServer::handle_cancel(
-    const std::shared_ptr<ConcatenateActionServer::GoalHandleConcatenate> goal_handle)
+rclcpp_action::CancelResponse ConcatenateActionServer::HandleCancel(
+    const std::shared_ptr<GoalHandleConcatenate> goalHandle)
 {
   RCLCPP_INFO(rclcpp::get_logger("server"), "Got request to cancel goal");
-  (void)goal_handle;
+  (void)goalHandle;
   return rclcpp_action::CancelResponse::ACCEPT;
 }
 
 
 void ConcatenateActionServer::execute(
-  const std::shared_ptr<ConcatenateActionServer::GoalHandleConcatenate> goal_handle)
+  const std::shared_ptr<GoalHandleConcatenate> goalHandle)
 {
   RCLCPP_INFO(rclcpp::get_logger("server"), "Executing the concatenation");
   rclcpp::Rate loop_rate(1);
-  const auto goal = goal_handle->get_goal();
+  const auto goal = goalHandle->get_goal();
   auto feedback = std::make_shared<Concatenate::Feedback>();
   std::string myString = "HELLOWORLD";
   auto &concatenation = feedback->partial_concatenation;
@@ -92,17 +91,17 @@ void ConcatenateActionServer::execute(
   for (int i = 1; (i < goal->num_concatenations) && rclcpp::ok(); ++i)
   {
     // check if there is a cancel request
-    if (goal_handle->is_canceling())
+    if (goalHandle->is_canceling())
     {
       result->final_concatenation = concatenation;
-      goal_handle->canceled(result);
+      goalHandle->canceled(result);
       RCLCPP_INFO(rclcpp::get_logger("server"), "Goal Canceled");
       return;
     }
     // update the final concatenation
     concatenation = concatenation + " " + myString;
     // update and publish feedback of the partial concatenation
-    goal_handle->publish_feedback(feedback);
+    goalHandle->publish_feedback(feedback);
     RCLCPP_INFO(rclcpp::get_logger("server"), "Publish Feedback");
 
     loop_rate.sleep();
@@ -112,14 +111,14 @@ void ConcatenateActionServer::execute(
   if (rclcpp::ok())
   {
     result->final_concatenation = concatenation;
-    goal_handle->succeed(result);
+    goalHandle->succeed(result);
     RCLCPP_INFO(rclcpp::get_logger("server"), "Goal Succeeded");
   }
 }
 
 
-void ConcatenateActionServer::handle_accepted(
-  const std::shared_ptr<ConcatenateActionServer::GoalHandleConcatenate> goal_handle)
+void ConcatenateActionServer::HandleAccepted(
+  const std::shared_ptr<GoalHandleConcatenate> goal_handle)
 {
   using namespace std::placeholders;
   // this needs to return quickly to avoid blocking the executor, so spin up a new thread
